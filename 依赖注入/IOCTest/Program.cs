ee22bf874910658d3f086489036cb7d7decd76b4
梁士ï¿?[user] <deawback@hotmail.com>
2016-03-20 22:24:26 +0800

    提交依赖注入练习代码

---
+++ 依赖注入/IOCTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCTest
+{
+    class Program
+    {
+
+        //容器实例
+        private static IContainer _container;
+        static void Main(string[] args)
+        {
+            InitIOC();
+            var lsw = _container.Resolve<GetLSW>();
+            Person per = lsw.Get();
+            Console.WriteLine(per.Name);
+
+        }
+
+
+        private static void InitIOC()
+        {
+            var builder = new ContainerBuilder();
+            //以接口的方式注册 
+            //实例一
+            builder.RegisterType<GetLSWbyHome>().AsImplementedInterfaces();
+            //实例二
+            builder.RegisterType<GetLSWbyWork>().AsImplementedInterfaces();
+            builder.RegisterType<GetLSW>();
+            _container = builder.Build();
+        }
+    }
+}
