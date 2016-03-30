df34444cb562c2630f4b1e661766367d35586d17
ShiWei-L <deawback@hotmail.com>
2016-03-31 07:05:53 +0800

    NG,BookStore应用框架 入口点 路由、依赖注入 app.js 控制器模块 controller.js 片段  tpls/ 框架搭建

---
+++ AngularDemo/Angular/BookStore/App/js/controllers.js
@@ -0,0 +1,14 @@
+﻿var bookStoreCtrls = angular.module('bookStoreCtrls', []);
+
+bookStoreCtrls.controller('HelloCtrl', function ($scope) {
+    $scope.name = "LSW";
+});
+
+bookStoreCtrls.controller('BoolListCtrl', function ($scope) {
+    $scope.books = [
+        { title: "围城", author: "钱钟书" },
+        { title: "嫌疑人X的献身", author: "东野圭吾" },
+        { title: "白夜行", author: "东野圭吾" },
+        { title: "山海经", author: "郭璞" }
+    ];
+});
