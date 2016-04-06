6f40246a96265c05e56de93c63b715d7d42e022c
ShiWei-L <deawback@hotmail.com>
2016-04-07 07:23:53 +0800

    提交指令交互学习代码

---
+++ AngularDemo/Angular/BookStore/指令/js/directiveJH.js
@@ -0,0 +1,73 @@
+﻿var app = angular.module('superHome', []);
+
+/*app.controller("superCtrl", function ($scope) {
+    $scope.loadData = function () {
+        console.log("这是我的超能力");
+    }
+});
+app.controller("superCtrl2", function ($scope) {
+    $scope.loadData2 = function () {
+        console.log("这是我的超能力-2");
+    }
+});
+
+app.directive("superPower", function () {
+    return {
+        restrict: "AE",
+        template: "<div>算天算地算自己</div>",
+        link: function (scope, element, attr) {
+            element.bind("mouseenter", function () {
+                scope.$apply(attr.power);
+            });
+        }
+    };
+})*/
+
+app.directive("superPower", function () {
+    return {
+        scope: {},
+        controller: function ($scope) {
+            $scope.powers = [];
+            this.addp1 = function () {
+                $scope.powers.push("吃饭");
+            };
+            this.addp2 = function () {
+                $scope.powers.push("睡觉");
+            };
+            this.addp3 = function () {
+                $scope.powers.push("写代码");
+            };
+        },
+        link: function (scope, element, attr) {
+            element.addClass("btn btn-primary");
+            element.bind("mouseenter", function () {
+                console.log(scope.powers);
+            });
+        }
+    }
+});
+
+app.directive("super1", function () {
+    return {
+        require: "^superPower",
+        link: function (scope, element, attr, superPowerCtrl) {
+            superPowerCtrl.addp1();
+        }
+    }
+})
+app.directive("super2", function () {
+    return {
+        require: "^superPower",
+        link: function (scope, element, attr, superPowerCtrl) {
+            superPowerCtrl.addp2();
+        }
+    }
+})
+app.directive("super3", function () {
+    return {
+        require: "^superPower",
+        link: function (scope, element, attr, superPowerCtrl) {
+            superPowerCtrl.addp3();
+        }
+    }
+})
