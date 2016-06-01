cc30e5246aea540488e0c4801e6edf86346b3f15
ShiWei-L <deawback@hotmail.com>
2016-06-01 22:25:42 +0800

    多线程部分结束

---
+++ C#学习笔记/Csharp学习笔记/第八章委托/19章多线程/同步锁.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 第八章委托._19章多线程
+{
+    class 同步锁
+    {
+        public static int Tickets { get; set; } = 100;
+        //辅助对象
+        public static object Obj = new object();
+
+        public void Main()
+        {
+            ThreadPool.QueueUserWorkItem(Spending);
+            ThreadPool.QueueUserWorkItem(Spending2);
+        }
+
+        public static void Spending(object obj)
+        {//消费一
+            while (true)
+            {
+                try
+                {
+
+                    Monitor.Enter(Obj);//获得排他锁
+                    if (Tickets > 0)
+                    {                       
+                        Console.WriteLine($"消费一 {Tickets--}");
+                    }
+                }
+                finally
+                {
+
+                    Monitor.Exit(Obj);//释放排他锁
+                }
+            };
+        }
+
+        public static void Spending2(object obj)
+        {//消费二
+            while (true)
+            {
+                try
+                {
+                    Monitor.Enter(Obj);//获得排他锁
+                    if (Tickets > 0)
+                    {
+                        Console.WriteLine($"消费二 {Tickets--}");
+                    }
+                }
+                finally
+                {
+
+                    Monitor.Exit(Obj);//释放排他锁
+                }
+            };
+        }
+    }
+}
