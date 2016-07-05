0a9c0921f4775bd16014c767db7de86d15a8f9bd
ShiWei-L <liangshiwei@shianyunduanbj.com>
2016-07-05 09:12:15 +0800

    提交一下dto转换demo代码

--- C#学习笔记/Csharp学习笔记/DTO转换测试/Program.cs
+++ C#学习笔记/Csharp学习笔记/DTO转换测试/Program.cs
@@ -1,40 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
 using EmitMapper;
+using AutoMapper;
+using Abp.AutoMapper;
+using Abp.Collections.Extensions;
+using Abp.Dependency;
+using Abp.Reflection;
 
 namespace DTO转换测试
 {
+    interface IPersonService
+    {
+        List<PersonDto> GetAll();
+    }
+    class PersonService : IPersonService
+    {
+        public List<PersonDto> GetAll()
+        {
+            return new List<PersonDto>() { new PersonDto() { Age = "23", Gender = "男", Name = "小明" } };
+        }
+    }
+
     class Program
     {
+        public IPersonService Service { get; set; }
+
+        public Program(IPersonService s)
+        {
+            IocManager ioc = new IocManager();
+
+            ioc.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+            this.Service = s;
+        }
+
         static void Main(string[] args)
         {
-            Mapper.Initialize(o => o.CreateMap<Person, PersonDto>());
+            new AutoConfig().Config();
+            //Mapper.CreateMap<Person, PersonDto>();
+            PersonDto qqq = new PersonDto();
+            Mapper.Initialize(o =>
+            {
+                o.CreateMap(typeof(Person), typeof(PersonDto));
+                o.CreateMap(typeof(PersonDto), typeof(Person));
+            });
 
-            Mapper.Initialize(o => o.CreateMap<PersonDto, Person>().ForMember(entity => entity.Name, opt => opt.Ignore()));
 
-            var mapper = ObjectMapperManager.DefaultInstance.GetMapper<Person, PersonDto>();
-            List<Person> per = new List<Person>() { new Person() { Age = "23", Gender = "男", Name = "小明", Pitem = new P1() { Gender = "24", Name = "小红" } } };
+
+            //Mapper.Initialize(o => o.CreateMap<PersonDto, Person>().ForMember(entity => entity.Name, opt => opt.Ignore()));
+
+            //var mapper = ObjectMapperManager.DefaultInstance.GetMapper<Person, PersonDto>();
+
+            //List<Person> per = new List<Person>() { new Person() { Age = "23", Gender = "男", Name = "小明" } };
+            // Person person = new Person() { Age = "23", Gender = "男", Name = "小明" };
             Stopwatch watch = new Stopwatch();
             watch.Start();
-            PersonDto dto = new PersonDto();
-            //PersonDto personDto = new PersonDto() { Age = "23", Gender = "男", Name = "小李" };
-            for (int i = 0; i < 100000; i++)
-            {
-                //PersonDto dto = ConvertToDto.ConvertTo<Person, PersonDto>(per, personDto);
-                dto = mapper.Map(per[0]);
-                //dto = Mapper.Map<PersonDto>(per[0]);
-            }
+
+            //PersonDto dto = person.MapTo<PersonDto>();//<PersonDto>();
+
+            PersonDto personDto = new PersonDto() { Age = "23", Gender = "男", Name = "小明" };
+            var per = Mapper.Map<Person>(personDto);
+            /* for (int i = 0; i < 100000; i++)
+             {
+                 //PersonDto dto = ConvertToDto.ConvertTo<Person, PersonDto>(per, personDto);
+                 dto = mapper.Map(per[0]);
+                 //dto = Mapper.Map<PersonDto>(per[0]);
+             }*/
             watch.Stop();
             Console.WriteLine(watch.ElapsedMilliseconds);
             Console.ReadKey();
         }
     }
+
+
 
     public class Person
     {
@@ -42,15 +85,16 @@ namespace DTO转换测试
         public string Age { get; set; }
         public string Gender { get; set; }
 
-        public P1 Pitem { get; set; }
+
     }
 
-    public class P1
+
+    public class Pitem
     {
         public string Name { get; set; }
         public string Gender { get; set; }
     }
-
+    
     public class PersonDto
     {
 
@@ -58,34 +102,48 @@ namespace DTO转换测试
         public string Age { get; set; }
         public string Gender { get; set; }
 
-        public string PitemName { get; set; }
-        public string PitemGender { get; set; }
+        //public string PitemName { get; set; }
+        //public string PitemGender { get; set; }
 
     }
 
-    public class ConvertToDto
+    public class AutoMapperAttribute : Attribute
     {
-        public static To ConvertTo<T, To>(T t, To to)
-        {
-            Type t2 = to.GetType();
-            Type t1 = t.GetType();
-            PropertyInfo[] pty = t1.GetProperties();
-            PropertyInfo[] pty2 = t2.GetProperties();
-            Object obj = Activator.CreateInstance(t2);
-            Object objs = Activator.CreateInstance(t1);
-            foreach (var propertyInfo in pty)
-            {
-                foreach (var propertyInfo2 in pty2)
-                {
-                    if (propertyInfo.Name.Equals(propertyInfo2.Name))
-                    {
-                        propertyInfo2.SetValue(obj, propertyInfo.GetValue(objs));
-                    }
-                }
-            }
-            return to;
+        public static List<Type> From { get; set; }
+        public static List<Type> To { get; set; }
 
+        public AutoMapperAttribute(Type from, Type to)
+        {
+            //From.Add(from);
+            // To.Add(to);
         }
+    }
+
+    public class AutoConfig
+    {
+        ITypeFinder _typeFinder = new TypeFinder();
+
+        public void Config()
+        {
+            var types = _typeFinder.Find(type =>
+                type.IsDefined(typeof(AutoMapperAttribute))
+                );
+
+            foreach (var type in types)
+            {
+                var obj = type.GetCustomAttributes<AutoMapperAttribute>();
+
+                /*if (obj.Length > 0)
+                {
+                    AutoMapperAttribute auto = (AutoMapperAttribute)obj[0];
+                }*/
+            }
+        }
+    }
+
+    public class Idto
+    {
+
     }
 
 }
