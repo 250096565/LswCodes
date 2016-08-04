f0c75c64764007c812787a5da4e884e1dc7744a8
ShiWei-L <deawback@hotmail.com>
2016-08-04 23:34:58 +0800

    提交UDP代码

---
+++ C#学习笔记/Csharp学习笔记/UDP网络通信/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace UDP网络通信
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLocalIP = new System.Windows.Forms.TextBox();
+            this.txtLocalPort = new System.Windows.Forms.TextBox();
+            this.txtSendPort = new System.Windows.Forms.TextBox();
+            this.txtSendIP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.txtConent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "本地IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "发送到";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "端口";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "端口";
+            // 
+            // txtLocalIP
+            // 
+            this.txtLocalIP.Location = new System.Drawing.Point(87, 25);
+            this.txtLocalIP.Name = "txtLocalIP";
+            this.txtLocalIP.Size = new System.Drawing.Size(152, 21);
+            this.txtLocalIP.TabIndex = 2;
+            // 
+            // txtLocalPort
+            // 
+            this.txtLocalPort.Location = new System.Drawing.Point(316, 25);
+            this.txtLocalPort.Name = "txtLocalPort";
+            this.txtLocalPort.Size = new System.Drawing.Size(98, 21);
+            this.txtLocalPort.TabIndex = 2;
+            // 
+            // txtSendPort
+            // 
+            this.txtSendPort.Location = new System.Drawing.Point(316, 74);
+            this.txtSendPort.Name = "txtSendPort";
+            this.txtSendPort.Size = new System.Drawing.Size(98, 21);
+            this.txtSendPort.TabIndex = 2;
+            // 
+            // txtSendIP
+            // 
+            this.txtSendIP.Location = new System.Drawing.Point(87, 74);
+            this.txtSendIP.Name = "txtSendIP";
+            this.txtSendIP.Size = new System.Drawing.Size(152, 21);
+            this.txtSendIP.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "内容 ";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(87, 121);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(268, 21);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(365, 121);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(29, 168);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(143, 168);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(365, 168);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 23);
+            this.btnReceive.TabIndex = 3;
+            this.btnReceive.Text = "接收";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // txtConent
+            // 
+            this.txtConent.Location = new System.Drawing.Point(12, 212);
+            this.txtConent.Multiline = true;
+            this.txtConent.Name = "txtConent";
+            this.txtConent.Size = new System.Drawing.Size(450, 161);
+            this.txtConent.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 385);
+            this.Controls.Add(this.txtConent);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtSendIP);
+            this.Controls.Add(this.txtSendPort);
+            this.Controls.Add(this.txtLocalPort);
+            this.Controls.Add(this.txtLocalIP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "UDP Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLocalIP;
+        private System.Windows.Forms.TextBox txtLocalPort;
+        private System.Windows.Forms.TextBox txtSendPort;
+        private System.Windows.Forms.TextBox txtSendIP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.TextBox txtConent;
+    }
+}
+
---
+++ C#学习笔记/Csharp学习笔记/UDP网络通信/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UDP网络通信.Properties;
+
+namespace UDP网络通信
+{
+    public partial class Form1 : Form
+    {
+
+        //发送客户端
+        private UdpClient sendUdpClient;
+
+        //接收客户端
+        private UdpClient ReceiveUdpClient;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //拿到指定主机的IP地址
+            IPAddress[] ips = Dns.GetHostAddresses("");
+
+            foreach (var ip in ips.Where(ip => ip.ToString().Contains("192")))
+            {
+                txtLocalIP.Text = ip.ToString();
+                txtSendIP.Text = ip.ToString();
+                break;
+            }
+
+            int port = 51883;
+            int sendPort = 11883;
+            txtLocalPort.Text = port.ToString();
+            txtSendPort.Text = sendPort.ToString();
+
+        }
+
+        /// <summary>
+        /// 接收消息按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            //创建接收套接字
+            IPAddress localIp = IPAddress.Parse(txtLocalIP.Text);
+
+            IPEndPoint localIpEndPoint = new IPEndPoint(localIp, int.Parse(txtLocalPort.Text));
+
+            try
+            {
+                ReceiveUdpClient = new UdpClient(localIpEndPoint);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Thread th = new Thread(ReceiveMessage);
+            th.Start();
+            //Task task = Task.Factory.StartNew(ReceiveMessage);
+            // task.Start();
+
+        }
+
+
+        /// <summary>
+        /// 接收消息方法
+        /// </summary>
+        private void ReceiveMessage()
+        {
+            if (ReceiveUdpClient == null)
+                return;
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                try
+                {
+                    //关闭receiveUdpClient时会产生异常
+                    byte[] receiveBytes = ReceiveUdpClient.Receive(ref remoteIpEndPoint);
+
+                    string message = Encoding.Unicode.GetString(receiveBytes);
+
+                    //显示消息内容
+                    ShowMessageForView(txtConent, $"{remoteIpEndPoint}{message}");
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        delegate void ShowMessageForViewCallBack(TextBox textbox, string text);
+
+
+        /// <summary>
+        /// 显示消息 
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <param name="text"></param>
+        private void ShowMessageForView(TextBox txt, string text)
+        {
+            if (txt.InvokeRequired)
+            {
+                ShowMessageForViewCallBack back = ShowMessageForView;
+
+                txt.Invoke(back, new object[] { txt, text });
+            }
+            else
+            {
+                txt.Text = DateTime.Now + Environment.NewLine + Resources.Form1_ShowMessageForView___ + text;
+            }
+        }
+
+        //发送消息按钮
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMessage.Text))
+            {
+                MessageBox.Show(Resources.Form1_btnSend_Click_消息不可以为空);
+                return;
+            }
+
+            IPAddress localIp = IPAddress.Parse(txtLocalIP.Text);
+            IPEndPoint localIpEndPoint = new IPEndPoint(localIp, int.Parse(txtLocalPort.Text));
+
+            sendUdpClient = new UdpClient(localIpEndPoint);
+
+            Thread task = new Thread(SendMessage);
+            task.Start(txtMessage.Text);
+        }
+
+
+        /// <summary>
+        /// 发送消息方法
+        /// </summary>
+        /// <param name="obj"></param>
+        private void SendMessage(object obj)
+        {
+            string message = (string)obj;
+
+            byte[] sendBytes = Encoding.Unicode.GetBytes(message);
+            IPAddress remoteIp = IPAddress.Parse(txtSendIP.Text);
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(remoteIp, int.Parse(txtSendPort.Text));
+            sendUdpClient.Send(sendBytes, sendBytes.Length, remoteIpEndPoint);
+
+            sendUdpClient.Close();
+
+            //请空消息
+            ResetMessage(txtMessage);
+        }
+
+
+        delegate void ResetMessageCallBack(TextBox text);
+
+        private void ResetMessage(TextBox txt)
+        {
+            //InvokeRequired属性代表如果处理空间与调用线程是在不同的线程上创建的
+            if (txt.InvokeRequired)
+            {
+                ResetMessageCallBack back = ResetMessage;
+                txt.Invoke(back, new object[] { txt });
+            }
+            else
+            {
+                txt.Clear();
+                txt.Focus();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (ReceiveUdpClient != null)
+                ReceiveUdpClient.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtConent.Clear();
+        }
+    }
+}
